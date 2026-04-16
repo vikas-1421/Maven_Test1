be09692f3bd8cea8b14ef4acd9a3180e4d056ab4
vikiccha2105-cpu <vikiccha2105@gmail.com>
2026-04-16 07:08:36 +0000

    Initial commit

---
+++ App.java
@@ -0,0 +1,13 @@
+package com.example;
+
+/**
+  *  
+  * 
+  */
+public class App
+{
+     public static void main( String[] args )
+     {
+         System.out.println( "vikas angadi ,1bi23cs244 this is the content of mainclass.java");
+     }
+}
